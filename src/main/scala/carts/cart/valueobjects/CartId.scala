38e0b5458f9bd35791c4f65191462fa1e26a1288
turbo-ismam <turboismam@gmail.com>
2023-01-20 19:45:38 +0100

    add CartId interface

--- src/main/scala/carts/cart/valueobjects/CartId.scala
+++ src/main/scala/carts/cart/valueobjects/CartId.scala
@@ -1,7 +1,13 @@
 package io.github.pervasivecats
 package carts.cart.valueobjects
 
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.NonNegative
+
+type id = Long Refined NonNegative
+
 trait CartId {
 
+  val value: id
 
 }
