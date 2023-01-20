dd7f5405d7ef824d72ac9f98d35fa3c2a009af0c
turbo-ismam <turboismam@gmail.com>
2023-01-20 19:56:39 +0100

    feat: add customer interface

---
+++ src/main/scala/carts/cart/valueobjects/Customer.scala
@@ -0,0 +1,14 @@
+package io.github.pervasivecats
+package carts.cart.valueobjects
+
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.string.MatchesRegex
+
+type EmailString = String Refined
+  MatchesRegex[
+    "[a-z0-9!#$%&'*+/=?^_`{|}~\\-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~\\-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+  ]
+trait Customer {
+
+  val value: EmailString
+}
