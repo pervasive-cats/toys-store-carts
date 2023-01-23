5b09ea87ad082ac0bd7c0da428b961e355415b87
Elena Rughi <elenarughi@gmail.com>
2023-01-23 20:23:27 +0100

    feat: add Store

--- src/main/scala/carts/cart/valueobjects/Store.scala
+++ src/main/scala/carts/cart/valueobjects/Store.scala
@@ -7,12 +7,28 @@
 package io.github.pervasivecats
 package carts.cart.valueobjects
 
+import eu.timepit.refined.api.RefType.applyRef
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.NonNegative
 
-import carts.Id
+import carts.{Id, Validated, ValidationError}
 
 trait Store {
 
   val value: Id
+}
+
+object Store {
+
+  private case class StoreImpl(value: Id) extends Store
+
+  case object WrongStoreFormat extends ValidationError {
+
+    override val message: String = "The store format is invalid"
+  }
+
+  def apply(value: Long): Validated[Store] = applyRef[Id](value) match {
+    case Left(_) => Left[ValidationError, Store](WrongStoreFormat)
+    case Right(value) => Right[ValidationError, Store](StoreImpl(value))
+  }
 }
