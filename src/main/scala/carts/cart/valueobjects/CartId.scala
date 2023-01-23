98e32f8e7768c36e03018d0238bf2ae6fdf78358
Elena Rughi <elenarughi@gmail.com>
2023-01-23 20:13:33 +0100

    feat: add CartId

--- src/main/scala/carts/cart/valueobjects/CartId.scala
+++ src/main/scala/carts/cart/valueobjects/CartId.scala
@@ -7,12 +7,28 @@
 package io.github.pervasivecats
 package carts.cart.valueobjects
 
+import eu.timepit.refined.api.RefType.applyRef
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.numeric.NonNegative
 
-import carts.Id
+import carts.{Id, Validated, ValidationError}
 
 trait CartId {
 
   val value: Id
+}
+
+object CartId {
+
+  private case class CartIdImpl(value: Id) extends CartId
+
+  case object WrongCartIdFormat extends ValidationError {
+
+    override val message: String = "The cart id format is invalid"
+  }
+
+  def apply(value: Long): Validated[CartId] = applyRef[Id](value) match {
+    case Left(_) => Left[ValidationError, CartId](WrongCartIdFormat)
+    case Right(value) => Right[ValidationError, CartId](CartIdImpl(value))
+  }
 }
---
+++ src/test/scala/carts/cart/valueobjects/CartIdTest.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2022-2023 by Pervasive Cats S.r.l.s.
+ *
+ * All Rights Reserved.
+ */
+package io.github.pervasivecats
+package carts.cart.valueobjects
+
+import eu.timepit.refined.auto.given
+import org.scalatest.EitherValues.given
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers.*
+
+import carts.cart.valueobjects.CartId.WrongCartIdFormat
+
+class CartIdTest extends AnyFunSpec {
+
+  describe("A cart id") {
+    describe("when created with a negative value identifier") {
+      it("should not be valid") {
+        CartId(-9000).left.value shouldBe WrongCartIdFormat
+      }
+    }
+
+    describe("when created with a positive value identifier") {
+      it("should be valid") {
+        val cartId: Long = 9000
+
+        (CartId(cartId).value.value: Long) shouldBe cartId
+      }
+    }
+
+    describe("when created with an identifier of value 0") {
+      it("should be valid") {
+        val cartId: Long = 0
+
+        (CartId(cartId).value.value: Long) shouldBe cartId
+      }
+    }
+  }
+}
